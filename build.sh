ad864737d5fc98f538d15780c4856368f519c921
oroce <robert@oroszi.net>
2016-11-02 22:11:28 +0100

    make sure we are in the right dir

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ node --version
 npm --version
 export PATH=./node_modules/.bin/:$PATH
 npm install -g node-pre-gyp-github
+cd /root/workspace
 npm install --build-from-source --node-gyp=$(which pangyp)
 ./node_modules/.bin/node-pre-gyp package
